b69fc07d4555f9812ee4978cc5c0dad728c95786
xest13 <aleix13t13m@gmail.com>
2016-03-20 18:43:42 +0100

    El lluc es gay

--- src/Domini/Estat.java
+++ src/Domini/Estat.java
@@ -5,11 +5,10 @@ import IA.DistFS.*;
 public class Estat {
 	private ArrayList<Integer> peticions;
 	private ArrayList<Integer> tempsServidors;
-	
+
 	public Estat(Requests req,Servers ser, int numGenIni){
 		peticions = new ArrayList<Integer>();
 		tempsServidors = new ArrayList<Integer>();
+		int elLLucEsgay = 1000;
 	}
-	
-	
 }

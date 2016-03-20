a87ec7faea9afad3447d45f28ff3080c4a4ac5df
lluc <lluc.bove.canals@gmail.com>
2016-03-20 20:22:05 +0100

    Error corrected: max instead of mean

--- src/Domini/Heuristic.java
+++ src/Domini/Heuristic.java
@@ -11,17 +11,19 @@ public class Heuristic  implements HeuristicFunction{
         Estat state = (Estat) o;
         int n = state.serversNum();
         double mean = 0.0;
+        double max = 0.0;
         double M2 = 0.0;
         double sd;
         for(int i = 1; i <= n ; ++i) {
             double t = state.getTime(i);
+            if( t > max ) max = t;
             double delta = t - mean;
             mean += delta / i;
             M2 += delta * (t - mean);
         }
         if (n < 2) sd = Double.NaN;
         else sd =  Math.sqrt(M2 / n);
-        return p_sd * sd + (1-p_sd) * mean;
+        return p_sd * sd + (1-p_sd) * max;
     }
 
 }

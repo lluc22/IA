2480fe1d5d6412a253476522170149946256977c
mayer13 <oscarfg13@gmail.com>
2016-03-20 20:06:49 +0100

    error operadors arreglat

--- src/Domini/Estat.java
+++ src/Domini/Estat.java
@@ -14,16 +14,16 @@ public class Estat {
 	public void assigna (int pet, int serv, Requests req, Servers ser) {
 		int usuari = req.getRequest(pet)[0];
 		int servAntic = peticions[pet];
-		tempsServidors[servAntic] -= ser.tranmissionTime(usuari, servAntic);
+		tempsServidors[servAntic] -= ser.tranmissionTime(servAntic, usuari);
 		peticions[pet] = serv;
-		tempsServidors[serv] += ser.tranmissionTime(usuari, serv);
+		tempsServidors[serv] += ser.tranmissionTime(serv, usuari);
 	}
 
 	public void intercanvia (int pet1, int pet2, Requests req, Servers ser) {
-		int tempsAntic1 = ser.tranmissionTime(req.getRequest(pet1)[0], peticions[pet1]);
-		int tempsAntic2 = ser.tranmissionTime(req.getRequest(pet2)[0], peticions[pet2]);
-		int tempsNou1 = ser.tranmissionTime(req.getRequest(pet1)[0], peticions[pet2]);
-		int tempsNou2 = ser.tranmissionTime(req.getRequest(pet2)[0], peticions[pet1]);
+		int tempsAntic1 = ser.tranmissionTime(peticions[pet1], req.getRequest(pet1)[0]);
+		int tempsAntic2 = ser.tranmissionTime(peticions[pet2], req.getRequest(pet2)[0]);
+		int tempsNou1 = ser.tranmissionTime(peticions[pet2], req.getRequest(pet1)[0]);
+		int tempsNou2 = ser.tranmissionTime(peticions[pet1], req.getRequest(pet2)[0]);
 		tempsServidors[peticions[pet1]] += tempsNou2 - tempsAntic1;
 		tempsServidors[peticions[pet2]] += tempsNou1 - tempsAntic2;
 		int aux = peticions[pet1];

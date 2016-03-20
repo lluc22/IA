b9f0690f768c6f973491ee5a35c800b4cbef0da0
lluc <lluc.bove.canals@gmail.com>
2016-03-20 18:41:06 +0100

    Added EstatFinal.java final version

---
+++ src/Domini/EstatFinal.java
@@ -0,0 +1,12 @@
+package Domini;
+
+/**
+ * Created by lluc on 20/03/16.
+ */
+import aima.search.framework.GoalTest;
+public class EstatFinal implements GoalTest{
+    @Override
+    public boolean isGoalState(Object o) {
+        return false;
+    }
+}

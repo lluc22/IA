df70bd2d1e89f6b4b1216e4551b75c127f87a5f0
mayer13 <oscarfg13@gmail.com>
2016-03-30 19:46:50 +0200

    potAssignar arreglat

--- src/Domini/Estat.java
+++ src/Domini/Estat.java
@@ -44,12 +44,10 @@ public class Estat {
 	public boolean potAssignar (int pet, int serv, Requests req, Servers ser) {
 		int fileId = req.getRequest(pet)[1];
 		Set<Integer> fileLocations = ser.fileLocations(fileId);
-		int servId = fileLocations.iterator().next();
 		boolean found = false;
-		while (fileLocations.iterator().hasNext() && !found) {
-			if (serv == servId) found = true;
-			servId = fileLocations.iterator().next();
-		}
+		for(Iterator it = fileLocations.iterator(); it.hasNext() && !found;){
+				if (serv == (int)it.next()) found = true;
+			}
 		return found;
 	}
 

ba3685dd6076e2b797a8773dbb6fc2954c3d57a5
mayer13 <oscarfg13@gmail.com>
2016-04-08 17:49:34 +0200

    bugs exp4 arreglat

--- src/Drivers/DriverExp4.java
+++ src/Drivers/DriverExp4.java
@@ -56,8 +56,8 @@ public class DriverExp4 {
                 }
                 PrintStream out = new PrintStream(new FileOutputStream("outputUsers.txt"));
                 System.setOut(out);
-                System.out.printf("Usuaris" + "\t" + "Temps");
-                for(int i = 0; i < tempsRep.length; ++i) System.out.printf((100 + i*100) +"\t" + tempsRep[i]/rep);
+                System.out.println("Usuaris" + "\t" + "Temps");
+                for(int i = 0; i < tempsRep.length; ++i) System.out.println((100 + i*100) +"\t" + tempsRep[i]/rep);
                 break;
             case 2:
                 System.out.print("Nombre d'iteracions: ");
@@ -87,8 +87,8 @@ public class DriverExp4 {
                 }
                 PrintStream out2 = new PrintStream(new FileOutputStream("outputServ.txt"));
                 System.setOut(out2);
-                System.out.printf("Servidors" + "\t" + "Temps");
-                for(int i = 0; i < tempsRep2.length; ++i) System.out.printf((50 + i*50) + "\t" + tempsRep2[i]/rep2);
+                System.out.println("Servidors" + "\t" + "Temps");
+                for(int i = 0; i < tempsRep2.length; ++i) System.out.println((50 + i*50) + "\t" + tempsRep2[i]/rep2);
                 break;
             case -1: System.exit(0);
         }
